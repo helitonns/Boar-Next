4d10ffc6cabe69590750e8120d94988b4a42f005
helitonns <helitonns@gmail.com>
2026-01-20 10:37:07 -0400

    Componentes de Seção

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
+import { Section } from "@/components/section";
 import { ArchiveIcon } from "lucide-react";
+
 
 export default function Home() {
   return (
@@ -6,19 +8,20 @@ export default function Home() {
       <div />
 
       <main className="grid grid-cols-4 gap-5 flex-1 items-stretch">
-        <div className="bg-navy-800 rounded-xl border-[0.5px] border-navy-500 pt-3 flex flex-col gap-1">
-          <div className="flex items-center justify-between px-3">
-            <span className="bg-navy-700 rounded-lg px-3 py-1.5 flex items-center gap-2 text-xs">
+
+        <Section.Root>
+          <Section.Header>
+            <Section.Title>
               <ArchiveIcon className="size-4" />
               Backlog
-            </span>
-            <span className="text-xs text-navy-200">32</span>
-          </div>
+            </Section.Title>
+            <Section.IssueCount>32</Section.IssueCount>
+          </Section.Header>
 
-          <div className="flex flex-col gap-2.5 overflow-y-scroll p-3">
+          <Section.Content>
             <div>card 1</div>
-          </div>
-        </div>
+          </Section.Content>
+        </Section.Root>
 
         
       </main>
