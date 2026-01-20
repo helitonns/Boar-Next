154ea484321af6b74b2a85d6e28f3adc2809b440
helitonns <helitonns@gmail.com>
2026-01-20 16:29:22 -0400

    Ajustes

--- src/app/(board)/page.tsx
+++ src/app/(board)/page.tsx
@@ -1,3 +1,7 @@
+import { Button } from "@/components/button";
+import { Card } from "@/components/card";
+import { Section } from "@/components/section";
+import { ArchiveIcon, MessageCircleIcon, ThumbsUpIcon } from "lucide-react";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -11,9 +15,38 @@ export default async function Board({ searchParams } : BoardProps) {
   const { q } = await searchParams;
 
   return (
-    <div>
-      Board
-    </div>
-  );
-
-}
+      <div className="max-w-405 w-full mx-auto p-10 flex flex-col gap-8 h-dvh">
+        <main className="grid grid-cols-4 gap-5 flex-1 items-stretch">
+          <Section.Root>
+            <Section.Header>
+              <Section.Title>
+                <ArchiveIcon className="size-4" />
+                Backlog
+              </Section.Title>
+              <Section.IssueCount>32</Section.IssueCount>
+            </Section.Header>
+  
+            <Section.Content>
+              <Card.Root href="/">
+                <Card.Header>
+                  <Card.Number>ECO-1234</Card.Number>
+                  <Card.Title>Design new landing page</Card.Title>
+                </Card.Header>
+                <Card.Footer>
+                  <Button>
+                    <ThumbsUpIcon className="size-3" />
+                    <span className="text-sm">12</span>
+                  </Button>
+                  <Button>
+                    <MessageCircleIcon className="size-3" />
+                    <span className="text-sm">6</span>
+                  </Button>
+                </Card.Footer>
+              </Card.Root>
+            </Section.Content>
+          </Section.Root>
+        </main>
+      </div>
+    );
+  }
+  
