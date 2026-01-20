fa5483b0b35217c1640da968effa4707fd893a0a
helitonns <helitonns@gmail.com>
2026-01-20 15:55:33 -0400

    Autenticação com Better Auth

--- src/app/(board)/header.tsx
+++ src/app/(board)/header.tsx
@@ -1,17 +1,28 @@
 "use client";
 
 import { Input } from "@/components/input";
-import { LogInIcon, SearchIcon } from "lucide-react";
+import { authClient } from "@/lib/auth-client";
+import { Loader2Icon, LogInIcon, SearchIcon } from "lucide-react";
 import { debounce, parseAsString, useQueryState } from "nuqs";
 import { ChangeEvent } from "react";
 
 export function Header() {
+  const { data: session, isPending } = authClient.useSession();
+
   const [search, setSearch] = useQueryState("q", parseAsString.withDefault(""));
 
   function handleSearchUpdate(event: ChangeEvent<HTMLInputElement>) {
     setSearch(event.target.value, {
       limitUrlUpdates: event.target.value !== "" ? debounce(350) : undefined,
     });
+  }
+
+  async function handleSignIn() {
+    authClient.signIn.social({ provider: "github", callbackURL: "/" });
+  }
+
+  async function handleSignOut() {
+    await authClient.signOut();
   }
 
   return (
@@ -23,23 +34,40 @@ export function Header() {
 
       <div className="flex items-center gap-4">
         <div className="relative">
-          <SearchIcon className="absolute size-4 text-navy-200 left-2.5 top-1/2 -translate-1/2 ml-2 pointer-events-none"/>
-          <Input 
-            type="text" 
-            placeholder="Search for features..." 
+          <SearchIcon className="absolute size-4 text-navy-200 left-2.5 top-1/2 -translate-1/2 ml-2 pointer-events-none" />
+          <Input
+            type="text"
+            placeholder="Search for features..."
             className="w-67.5 pl-8"
             value={search}
             onChange={handleSearchUpdate}
           />
         </div>
 
-        <button 
-          type="button" 
-          className="size-8 rounded-full bg-navy-700 border border-navy-500 flex items-center justify-center
+        {isPending ? (
+          <div className="size-8 rounded-full bg-navy-700 border border-navy-500 flex items-center justify-center">
+            <Loader2Icon className="size-3.5 text-navy-200 animate-spin"/>
+          </div>
+        ) : session?.user ? (
+            <button
+              type="button"
+              className="size-8 rounded-full overflow-hidden cursor-pointer"
+              onClick={handleSignOut}
+            >
+              <img src={session.user.image ?? ""} alt={session.user.name} className="size-8 rounded-full"/>
+            </button>
+        ) : (
+          <button
+            type="button"
+            className="size-8 rounded-full bg-navy-700 border border-navy-500 flex items-center justify-center
             hover:bg-navy-600 transition-colors duration-150 cursor-pointer"
-        >
-          <LogInIcon className="size-3.5 text-navy-200"/>
-        </button>
+            onClick={handleSignIn}
+          >
+            <LogInIcon className="size-3.5 text-navy-200" />
+          </button>
+        )
+        }
+
       </div>
     </div>
   );
