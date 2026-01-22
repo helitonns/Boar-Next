45ee24d5c0bd20bc61d8b46d6e4aba3f1d1a6c31
helitonns <helitonns@gmail.com>
2026-01-22 09:29:04 -0400

    Atualização de cache dos likes

--- src/components/like-button.tsx
+++ src/components/like-button.tsx
@@ -2,7 +2,7 @@ import type { IssueInteractionsResponseSchema } from "@/api/routes/schemas/issue
 import { toogleLike } from "@/http/toogle-like";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { ThumbsUpIcon } from "lucide-react";
-import { ComponentProps } from "react";
+import { ComponentProps, MouseEvent } from "react";
 import z from "zod";
 import { Button } from "./button";
 
@@ -16,38 +16,51 @@ interface LikeButtonProps extends ComponentProps<"button"> {
 export function LikeButton({ issueId, initialLikes, initialLiked = false, ...props }: LikeButtonProps) {
   const queryClint = useQueryClient();
 
-  const { mutate: handleToogleLIke, isPending } = useMutation({
-    mutationFn: () => toogleLike({issueId}),
-    onMutate: async ()=> {
-      const previousData = queryClint.getQueryData<IssueInteractionResponse>(["issue-likes", issueId]);
-      queryClint.setQueryData<IssueInteractionResponse>(["issue-likes", issueId], old => {
-        if(!old) return undefined;
-
-        return {
-          ...old,
-          interactions: old.interactions.map(interaction => {
-            if (interaction.issueId === issueId){
-              return {
-                ...interaction,
-                isLiked: !interaction.isLiked,
-                likesCount: interaction.isLiked ? interaction.likesCount - 1 : interaction.likesCount + 1
-              }
-            }
-            return interaction;
-          }) 
-        }
+  const { mutate: onToogleLIke, isPending } = useMutation({
+    mutationFn: () => toogleLike({ issueId }),
+    onMutate: async () => {
+      const previousData = queryClint.getQueriesData<IssueInteractionResponse>({
+        queryKey: ["issue-likes"]
       });
+
+      queryClint.setQueriesData<IssueInteractionResponse>(
+        { queryKey: ["issue-likes"] },
+        old => {
+          if (!old) return undefined;
+
+          return {
+            ...old,
+            interactions: old.interactions.map(interaction => {
+              if (interaction.issueId === issueId) {
+                return {
+                  ...interaction,
+                  isLiked: !interaction.isLiked,
+                  likesCount: interaction.isLiked ? interaction.likesCount - 1 : interaction.likesCount + 1
+                }
+              }
+              return interaction;
+            })
+          }
+        });
 
       return { previousData }
     },
-    onError: async (_err, _params, context)=> {
-      if(context?.previousData){
-        queryClint.setQueryData<IssueInteractionResponse>(["issue-likes", issueId], context.previousData);
+    onError: async (_err, _params, context) => {
+      if (context?.previousData) {
+        for(const [queryKey, data] of context.previousData){
+          queryClint.setQueryData<IssueInteractionResponse>(queryKey, data);
+        }
       }
     }
   });
 
   const liked = initialLiked;
+
+  function handleToggleLike(event: MouseEvent<HTMLButtonElement>) {
+    event.preventDefault();
+    event.stopPropagation();
+    onToogleLIke();
+  }
 
   return (
     <Button
@@ -55,7 +68,7 @@ export function LikeButton({ issueId, initialLikes, initialLiked = false, ...pro
       className="data-[liked=true]:bg-indigo-600 data-[liked=true]:text-white data-[liked=true]:hover:bg-indigo-500"
       aria-label={liked ? "Unlike" : "Like"}
       disabled={isPending}
-      onClick={() => handleToogleLIke()}
+      onClick={handleToggleLike}
       {...props}
     >
       <ThumbsUpIcon className="size-3" />
